b19e505aa61825027c03439cd78824a80ddbbf52
GuerdonL <guerdon@ligon.org>
2022-10-15 19:52:09 -0400

    added a border highlight effect on hover

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+// variable to hold previous parent div border
+var prevBorder = null;
 //listen for a click on the webpage
 document.addEventListener('click', function(e) {
     //get the element that was clicked
@@ -6,4 +8,24 @@ document.addEventListener('click', function(e) {
     var parent = element.parentElement;
     //set the background color of the parent div to green
     parent.style.backgroundColor = "green";
+});
+// listen for hover on the webpage
+document.addEventListener('mouseover', function(e) {
+    //get the element that was hovered
+    var element = e.target;
+    //get the parent div of the element that was hovered
+    var parent = element.parentElement;
+    // assign the previous parent div border to a variable
+    prevBorder = parent.style.border;
+    // set the border of the parent div to 2px solid green
+    parent.style.border = "2px solid green";
+});
+// listen for hover off the webpage
+document.addEventListener('mouseout', function(e) {
+    // get the element that was hovered off
+    var element = e.target;
+    // get the parent div of the element that was hovered off
+    var parent = element.parentElement;
+    // set the border of the parent div to the previous border
+    parent.style.border = prevBorder;
 });
